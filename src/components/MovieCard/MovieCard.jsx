8a683caf8d8128084f7d383dc26b39613975ac47
Artem <bezkorovainy2006@gmail.com>
2024-04-26 01:13:59 +0200

    added css code for moviecard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,16 +1,17 @@
-
+import './MovieCard.css';
 
 const MovieCard = ({ movie }) => {
     return (
-        <div className="card">
-        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
-            alt={movie.title} className="movie-img" />
-            <div className="p-4">
-                <h2 className="movie-title">{movie.title}</h2>
-                <p className="movie-overview">{movie.overview}</p>
-            </div>
-            <div className="movie-votes">
-                <span className="">{movie.vote_average}</span>
+        <div className="movie-card">
+            <div className='movie-card-flipped'>
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
+                alt={movie.title} className="movie-img" />
+                <div className="info">
+                    <h2 className="movie-title">{movie.title}</h2>
+                </div>
+                <div className="movie-votes">
+                    <span className="">{movie.vote_average}</span>
+                </div>
             </div>
         </div>
     )
