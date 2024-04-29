ac96f7c3efffb4afe80145b82063753b4ee9c907
Artem <bezkorovainy2006@gmail.com>
2024-04-29 17:45:18 +0200

    WIP css code, added some updates

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -8,12 +8,6 @@ const MovieCard = ({ movie }) => {
                 <div className='movie-card-flipped'>
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
                     alt={movie.title} className="movie-img" />
-                    <div className="info">
-                        <h2 className="movie-title">{movie.title}</h2>
-                    </div>
-                    <div className="movie-votes">
-                        <span className="">{movie.vote_average}</span>
-                    </div>
                 </div>
             </div>
         </Link>
