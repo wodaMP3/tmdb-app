3de9162f5e2c97956f7da5f6873645f999d0c34e
Artem <bezkorovainy2006@gmail.com>
2024-05-03 19:43:53 +0200

    edited loginPage, WIP

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -11,13 +11,32 @@ const bearer = import.meta.env.VITE_BEARER;
 
 const LoginPage = () => {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleSumbit = async (event) => {
+        event.preventDefault();
+    }
+
     const [data, setData] = useState([]);
     const isAunthenticated = useContext(AuthContext);
 
     useEffect(() => {
-        const fetchData = async() => {
+        const fetchData = async(e) => {
+            e.preventDefault();
+
             try {
                 const options = {
+                    email: email,
+                    password: password,
                     method: 'POST',
                     url: authURL,
                     headers: {
@@ -45,11 +64,11 @@ const LoginPage = () => {
     return (
         <div className="login">
             <h2 className="login-header">Login</h2>
-            <form>
+            <form onSubmit={handleSumbit}>
                 <label htmlFor="email">Email: </label>
-                <input className='email-input' type="email" id="email" required/>
+                <input className='email-input' type="email" id="email" value={email} onChange={handleEmailChange} required/>
                 <label htmlFor="password">Password: </label>
-                <input className='password-input' type="password" name="password" id="password" required/>
+                <input className='password-input' type="password" name="password" id="password" value={password} onChange={handlePasswordChange} required/>
                 <button>login</button>
                 <div>
                     <p>
