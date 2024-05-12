04b4c4b0f7dc89ae5af07a301c8fbb9618d55a45
Artem <bezkorovainy2006@gmail.com>
2024-05-12 18:32:32 +0200

    edited addFavourites

--- frontend/src/components/AddFavourites/AddFavourites.jsx
+++ frontend/src/components/AddFavourites/AddFavourites.jsx
@@ -5,41 +5,55 @@ import axios from "axios";
 const favURL = `https://api.themoviedb.org/3/account/{account_id}/favorite`;
 const bearer = import.meta.env.VITE_BEARER;
 
-const AddFavourites = ({ movie, onAddToFavourites }) => {
+// const AddFavourites = ({ movie, onAddToFavourites }) => {
 
-	const [data, setData] = useState(null);
+
+
+
+
+// 	const [data, setData] = useState(null);
 	
-	const handleFavourites = async () => {
+// 	const handleFavourites = async () => {
 
-		const fetchFavourites  = {
-		method: 'POST',
-		url: favURL, 
-		headers: {
-			account_id: 21235577,
-			accept: 'application/json',
-			'content-type': 'application/json',
-			Authorization: `Bearer ${bearer}`
-		}
-	}
+// 		const fetchFavourites  = {
+// 		method: 'POST',
+// 		url: favURL, 
+// 		headers: {
+// 			account_id: 21235577,
+// 			accept: 'application/json',
+// 			'content-type': 'application/json',
+// 			Authorization: `Bearer ${bearer}`
+// 		}
+// 	}
 
+// 	try {
+// 		const response = await axios.request(fetchFavourites);
+// 		setData(response.data);
+// 		console.log('Fetching favourites page is successfull:', response.data);
+// 	} catch (error) {
+// 		console.error('Failed to fetch favourites page:', error);
+// 	}
+// }	
+	
+export const AddFavourites = async (userId, movieId, movieData) => {
+	const db = getFirestore();
 	try {
-		const response = await axios.request(fetchFavourites);
-		setData(response.data);
-		console.log('Fetching favourites page is successfull:', response.data);
+		await addDoc(Collection(db, `users/${userId}/favourites`), {
+			movieId: movieId,
+			movieData: movieData
+		});
+		console.log('AddFavourites success');
 	} catch (error) {
-		console.error('Failed to fetch favourites page:', error);
+		console.error('failed to add favourites:', error)
 	}
-}	
 	
-
-
     return (
-        <div className="favs-page">
+		<div className="favs-page">
 
 		
             <span className="favourite" 
 				onClick={handleFavourites}
-			>Add to</span>
+				>Add to</span>
             <svg
 				width='1em'
 				height='1em'
@@ -56,5 +70,6 @@ const AddFavourites = ({ movie, onAddToFavourites }) => {
         </div>
     )
 }
+
 
 export default AddFavourites;
