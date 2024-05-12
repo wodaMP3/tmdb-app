34c74d16511eb5c5e778a6777c9ca2127b996df4
Artem <bezkorovainy2006@gmail.com>
2024-05-12 18:37:09 +0200

    edited favourites page

--- frontend/src/pages/FavouritesPage/FavouritesPage.jsx
+++ frontend/src/pages/FavouritesPage/FavouritesPage.jsx
@@ -1,24 +1,20 @@
+import { query } from 'express';
 import './FavouritesPage.css';
 
-const FavouritesPage = ({ favourites }) => {
-
-    if (!favourites || favourites.length === 0) {
-        return <p className="no-favs">No fav movies</p>
-    }
-
-    return (
-        <div className="favourites-page">
-      <h1>Избранные фильмы</h1>
-      <div className="movie-list">
-        {favourites.map((movie) => (
-          <div key={movie.id} className="movie-card">
-            <img src={movie.poster} alt={movie.title} />
-            <h2>{movie.title}</h2>
-          </div>
-        ))}
-      </div>
-    </div>
-    );
-} 
+const FavouritesPage = async (userId) => {
+  const db = getFirestore();
+  try {
+    const q = query(collection(db, `users/${userId}/favourites`));
+    const querySnapshot = await getDocs(q);
+    const favoriteMovies = [];
+    querySnapshot.forEach((doc) => {
+      favoriteMovies.push(doc.data());
+    });
+    return favoriteMovies;
+  } catch (error) {
+    console.error('failed to load fav movies:', error)
+    return [];
+  }
+}
 
 export default FavouritesPage;
